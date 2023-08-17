00904c6daf068fae3954d07cc13bc89de50ad73f
jn <janunezc@users.noreply.github.com>
2023-08-16 20:42:17 -0600

    Update download-table-as-csv.js

--- download-table-as-csv.js
+++ download-table-as-csv.js
@@ -1,16 +1,32 @@
 // ==UserScript==
 // @name         Table to CSV Downloader
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      2
 // @description  Download table content as CSV
 // @author       Mr. Nunez
 // @match        *://*/*  // This will match all URLs
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
-console.log("Tampemonkey for CSV Download tables Activated");
-setTimeout(function(){
+
+setTimeout(function() {
     'use strict';
-    console.log("5 Seconds Rule Triggered");
+
+    GM_addStyle(`
+        .custom-context-menu {
+            display: none;
+            position: absolute;
+            z-index: 1000;
+            background-color: white;
+            border: 1px solid #ccc;
+        }
+        .custom-context-menu-item {
+            padding: 5px 10px;
+            cursor: pointer;
+        }
+        .custom-context-menu-item:hover {
+            background-color: #f0f0f0;
+        }
+    `); // CSS for the context menu
 
     function downloadCSV(csv, filename) {
         var csvFile = new Blob([csv], { type: "text/csv" });
@@ -28,24 +44,39 @@ setTimeout(function(){
 
         for (var i = 0; i < rows.length; i++) {
             var row = [], cols = rows[i].querySelectorAll("td, th");
-            for (var j = 0; j < cols.length; j++){
+            for (var j = 0; j < cols.length; j++)
                 row.push('"' + cols[j].innerText.replace(/"/g, '""') + '"'); // Wrap in quotes and escape existing quotes
-            }
+
             csv.push(row.join(","));
         }
         downloadCSV(csv.join("\n"), filename);
     }
 
+    var contextMenu = document.createElement('div');
+    contextMenu.className = 'custom-context-menu';
+    var menuItem = document.createElement('div');
+    menuItem.className = 'custom-context-menu-item';
+    menuItem.textContent = 'Download CSV';
+    contextMenu.appendChild(menuItem);
+    document.body.appendChild(contextMenu);
+
     var tables = document.querySelectorAll('table');
 
     tables.forEach(function(table, index) {
-        console.log("Adding button on table", table, index);
-        var button = document.createElement('button');
-        button.textContent = 'Download CSV';
-        button.className = 'btn btn-default';
-        button.addEventListener('click', function () {
-            exportTableToCSV(table, 'table' + index + '.csv');
+        table.addEventListener('contextmenu', function (e) {
+            e.preventDefault(); // Prevent the default context menu
+            contextMenu.style.left = e.pageX + 'px';
+            contextMenu.style.top = e.pageY + 'px';
+            contextMenu.style.display = 'block';
+            menuItem.onclick = function() {
+                exportTableToCSV(table, 'table' + index + '.csv');
+                contextMenu.style.display = 'none'; // Hide the menu after clicking
+            };
         });
-        table.insertAdjacentElement('beforebegin', button);
+    });
+
+    // Hide the custom context menu when clicking elsewhere
+    window.addEventListener('click', function () {
+        contextMenu.style.display = 'none';
     });
 },5000);
